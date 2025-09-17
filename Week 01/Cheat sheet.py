a51bdc4dd2982f986a65b1eb12718a0c74223268
CasperRosenqvist <casper.rosenqvist@satudent.lab.fi>
2025-09-17 21:18:13 +0300

    17.9.2025

---
+++ Week 01/Cheat sheet.py
@@ -0,0 +1,30 @@
+print("Hello World!")
+
+print("He said: \"Hello\", ank kept walking")
+
+examplestring = "Text and numb3rs"
+exampleInt = 12
+examplefloat = 12.12
+exampleBoolean = False
+
+#Firstname = input("What is your firstname: ")
+#Lastname = input("What is your lastname: ")
+#print("Hello", Firstname, Lastname)
+
+Sentence = "Finding substring"
+
+# print(Sentence)
+# print(Sentence [0]) # Antaa lauseen ensimmäisen merkin
+# print(Sentence [-2]) # Antaa lauseen toiseksi viimeisen kirjaimen
+# print(Sentence[2:9]) # Antaa kirjaimet 2-9
+# print(Sentence[:9]) # Antaa 9 kiraimeen asti
+# print(Sentence[-9:-2]) # 9 lopusta - 2 lopusta 
+# print(Sentence[2:9:3]) # Aloitetaan 2 edetään 9 asti, 3 askeleen välit
+# print(Sentence[::3]) # Joka kolmas kirjain
+# print(Sentence[::-1]) # Askeleen välein lopusta lähtien
+
+num1 =input("Anna luku 1 ")
+num2 =input("Anna luku 2 ")
+
+num3= int(num1) + int(num2)
+print(num3)
