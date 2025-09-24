fb7b3d8361889d90dee34cabc323562becbdefdc
CasperRosenqvist <casper.rosenqvist@satudent.lab.fi>
2025-09-24 21:08:15 +0300

    24.9.2025

--- Week 02/A2_T5.py
+++ Week 02/A2_T5.py
@@ -13,12 +13,18 @@
 #The word 'Moonbanana' sliced to the defined substring is 'Moon'.
 #Program ending.
 
-print("Program starting. \n")
+print("Program starting.\n")
 
 Compound = input("Insert a closed compound word: ")
-print(f"The word you inserted is '{Compound}' and in reverse it is '{Compound [::-1]}'.")
-print(f"The inserted word length is {len(Compound)}")
-print(f"Last character is '{Compound [-1]}'\n")
+Reverse = Compound[::-1]
+
+print(f"The word you inserted is '{Compound}' and in reverse it is '{Reverse}'.")
+
+Length = len(Compound)
+print(f"The inserted word length is {Length}")
+
+Last = Compound[-1]
+print(f"Last character is '{Last}'\n")
 
 print("Take substring from the inserted word by inserting...")
 Starting_point = int(input("1) Starting point: "))
@@ -26,5 +32,5 @@ Ending_point = int(input("2) Ending point: "))
 Step_size = int(input("3) Step size: "))
 
 Substring = Compound[Starting_point:Ending_point:Step_size] 
-print(f"\n The word '{Compound}' sliced to defined substring is '{Substring}'")
+print(f"\nThe word '{Compound}' sliced to the defined substring is '{Substring}'.")
 print("Program ending.")
--- Week 02/A2_T6.py
+++ Week 02/A2_T6.py
@@ -8,13 +8,17 @@
 
 #Program ending.
 
-print("Program starting.")
+print("Program starting.\n")
 Color = input("Insert a hex color: ")
 
 Red = Color[1:3]
 Green = Color[3:5]
 Blue = Color[5:7]
 
-print(f"\nColors \n - Red {Red} \n - Green {Green} \n - Blue {Blue}")
+
+print("\nColors")
+print(f"- Red {Red}")
+print(f"- Green {Green}")
+print(f"- Blue {Blue}")
 
 print("\nProgram ending.")
