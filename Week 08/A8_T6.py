944616849cefc3101295525b61aa86479228b7cb
CasperRosenqvist <casper.rosenqvist@student.lab.fi>
2025-12-11 21:03:56 +0200

    Update A8_T6.py

--- Week 08/A8_T6.py
+++ Week 08/A8_T6.py
@@ -1,59 +1,51 @@
 
-import svgwrite
-from svgwrite import Drawing, cm
-from svgwrite.shapes import Rect, Circle, Polygon
-
-
-def drawSquare(PDwg: Drawing) -> None:
-    print("Insert square")
-    x = int(input("--Left edge position: "))
-    y = int(input("--Top edge position: "))
-    side = int(input("--Side length: "))
-    fill = input("--Fill color: ")
-    stroke = input("--Stroke color: ")
-    PDwg.add(Rect(insert=(x, y), size=(side, side), fill=fill, stroke=stroke))
-
-
-def drawCircle(PDwg: Drawing) -> None:
-    print("Insert circle")
-    cx = int(input("--Center x coord: "))
-    cy = int(input("--Center y coord: "))
-    r = int(input("--Radius: "))
-    fill = input("--Fill color: ")
-    stroke = input("--Stroke color: ")
-    PDwg.add(Circle(center=(cx, cy), r=r, fill=fill, stroke=stroke))
-
-
-def saveSvg(PDwg: Drawing) -> None:
-    filename = input("Insert filename: ")
-    print(f'Saving file to "{filename}"')
-    confirm = input("Proceed (y/n)?: ")
-    if confirm.lower() == "y":
-        PDwg.saveas(filename, pretty=True)
-        print("Vector saved successfully!")
-
+from drawLib import drawCircle, drawSquare, saveSvg, Drawing
 
 def main() -> None:
+    Dwg = Drawing()
     print("Program starting.")
-    Dwg = svgwrite.Drawing(size=(400, 200))  # Canvas size
     while True:
-        print("Options:")
-        print("1--Draw square")
-        print("2--Draw circle")
-        print("3--Save svg")
-        print("0--Exit")
-        choice = input("Your choice: ")
-        if choice == "1":
-            drawSquare(Dwg)
-        elif choice == "2":
-            drawCircle(Dwg)
-        elif choice == "3":
-            saveSvg(Dwg)
-        elif choice == "0":
-            print("Exiting program.")
-            break
-    print("\nProgram ending.")
+        showOptions()
+        choice = askChoice()
+        match choice:
+            case 1:
+                print('Insert square')
+                left = int(askValue1("Left edge position"))
+                top = int(askValue1("Top edge position"))
+                size = int(askValue1("Size"))
+                drawSquare(Dwg, left, top, size)
+            case 2:
+                print('Insert circle')
+                cx = int(askValue1("Center X position"))
+                cy = int(askValue1("Center Y position"))
+                r = int(askValue1("Radius"))
+                drawCircle(Dwg, cx, cy, r)
+            case 3:
+                filename = askValue2("Filename")
+                confirm = askValue2("Confirm save (y/n)")
+                if confirm.lower() == "y":
+                    saveSvg(Dwg, filename)
+            case 0:
+                print("Exiting program.\n")
+                break
+        print()
+    print("Program ending.")
 
+def showOptions() -> None:
+    print("Options:")
+    print("1 - Draw square")
+    print("2 - Draw circle")
+    print("3 - Save svg")
+    print("0 - Exit")
+
+def askChoice() -> int:
+    return int(input("Your choice: "))
+
+def askValue1(PPrompt: str) -> str:
+    return input(f"- {PPrompt}: ")
+
+def askValue2(PPrompt: str) -> str:
+    return input(f"{PPrompt}: ")
 
 if __name__ == "__main__":
     main()
