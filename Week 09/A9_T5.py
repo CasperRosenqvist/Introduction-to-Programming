e94d78efe8eb92bd8a8aad779e6e9ae15c2043ec
CasperRosenqvist <casper.rosenqvist@student.lab.fi>
2025-12-11 21:06:11 +0200

    Update A9_T5.py

--- Week 09/A9_T5.py
+++ Week 09/A9_T5.py
@@ -2,32 +2,39 @@
 RGB_MIN = 0
 RGB_MAX = 255
 
-def collector(name: str) -> int:
-    feed = input(f"Insert {name}: ")
+def askIntByte(PPrompt: str) -> int:
+    feed = input(PPrompt)
     try:
-        value = int(feed)
+        value = float(feed)
     except ValueError:
-        raise ValueError(f"'{feed}' is non-numeric value. \n Couldn't perform the designed task due to the invalid input values. ")
+        raise ValueError(f"'{feed}' is non-numeric value.")
+    if not value.is_integer():
+        raise ValueError(f"'{feed}' is not an integer.")
+    value = int(value)
     if value < RGB_MIN or value > RGB_MAX:
-        raise Exception(f'Value "{value}" is out of range 0-255.')
+        raise ValueError(f'Value "{value}" is out of range 0–255.')
     return value
 
-def main() -> None:
+def createHex(PRed: int, PGreen: int, PBlue: int) -> str:
+    return "#{:02x}{:02x}{:02x}".format(PRed, PGreen, PBlue)
+
+def main():
     print("Program starting.")
     try:
-        r = collector("red")
-        g = collector("green")
-        b = collector("blue")
+        r = askIntByte("Insert red: ")
+        g = askIntByte("Insert green: ")
+        b = askIntByte("Insert blue: ")
+        hexcode = createHex(r, g, b)
         print("RGB Details:")
         print(f"- Red: {r}")
         print(f"- Green: {g}")
         print(f"- Blue: {b}")
-        hexcode = "{:02x}{:02x}{:02x}".format(r, g, b)
-        print(f"- Hex: #{hexcode}")
+        print(f"- Hex: {hexcode}")
+        print(f"- Binary: {r:08b} {g:08b} {b:08b}")
     except Exception as err:
         print(err)
+        print("Couldn't perform the designed task due to the invalid input values.")
     print("Program ending.")
-    return None
 
-
-main()
+if __name__ == "__main__":
+    main()
