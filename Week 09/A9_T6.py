210c4e433d1b31d7554d1f0a6811942ef85614dc
CasperRosenqvist <casper.rosenqvist@student.lab.fi>
2025-12-11 21:07:54 +0200

    Update A9_T6.py

--- Week 09/A9_T6.py
+++ Week 09/A9_T6.py
@@ -1,45 +1,60 @@
 
-def printOptions() -> None:
+def showOptions() -> None:
     print("Options:")
     print("1 - Insert line")
     print("2 - Save lines")
     print("0 - Exit")
 
-def insertLine(Lines: list[str]) -> None:
-    Text = input("Insert text:")
-    Lines.append(Text)
+def askChoice() -> int:
+    try:
+        return int(input("Your choice: "))
+    except ValueError:
+        return -1
 
-def saveLines(Lines: list[str]) -> None:
-    Filename = input("Insert filename:")
-    with open(Filename, "w") as Filehandle:
-        for Line in Lines:
-            Filehandle.write(Line + "\n")
+def saveLines(PLines: list[str]) -> None:
+    try:
+        filename = input("Insert filename: ")
+        with open(filename, "w") as filehandle:
+            for line in PLines:
+                filehandle.write(line + "\n")
+    except Exception as err:
+        print(err)
+
+def insertLine(PLines: list[str]) -> None:
+    text = input("Insert text: ")
+    PLines.append(text)
+
+def onInterrupt(PLines: list[str]) -> None:
+    try:
+        if len(PLines) > 0:
+            answer = input("Save before quit (y/n?): ")
+            if answer.lower() == "y":
+                saveLines(PLines)
+    except Exception as err:
+        print(err)
 
 def main() -> None:
-    print("Program starting.")
     Lines: list[str] = []
-    while True:
-        printOptions()
-        try:
-            Choice = input("Your choice:")
-        except KeyboardInterrupt:
-            print("^CKeyboard interrupt and unsaved progress!")
-            if len(Lines) > 0:
-                Answer = input("Save before quit(y/n?):")
-                if Answer.lower() == "y":
-                    saveLines(Lines)
-            break
-
-        if Choice == "1":
-            insertLine(Lines)
-        elif Choice == "2":
-            saveLines(Lines)
-        elif Choice == "0":
-            break
-        else:
-            print("unknown option!")
-
+    Choice = -1
+    print("Program starting.")
+    try:
+        while Choice != 0:
+            showOptions()
+            Choice = askChoice()
+            if Choice == 1:
+                insertLine(Lines)
+            elif Choice == 2:
+                saveLines(Lines)
+            elif Choice == 0:
+                print("Exiting program.")
+            else:
+                print("Unknown option!")
+            print("")
+    except KeyboardInterrupt:
+        print("^C Keyboard interrupt and unsaved progress!")
+        onInterrupt(Lines)
+    Lines.clear()
     print("Program ending.")
-    return None
 
-main()
+if __name__ == "__main__":
+    main()
